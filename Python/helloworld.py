90a913947ae4745fe460e659611ed2acde247aef
Ashwani132003 <ashwanisiwach132003@gmail.com>
2021-10-05 14:41:12 +0530

    added hello wrold in python and added python resources

---
+++ Python/helloworld.py
@@ -0,0 +1,10 @@
+
+# First Program
+# In python print is used to print something as output.
+
+print("Hello, World!")
+
+# This will give an ouput as:
+# Hello, World! 
+
+#Congratulations for your first Python Program
